19bb47ef44ad74df566cdb1d16238940675b8bb5
Doppon <zaxpkuraudo7777@gmail.com>
2020-05-20 15:36:34 +0900

    擬似コードのサンプルプログラムを追加

--- sample.py
+++ sample.py
@@ -1,1 +1,21 @@
 # LTL のサンプルプログラム
+
+# パス上の次の状態で f が成り立つ
+def next_(f):
+    #
+    return True
+
+# パス上のある状態でいつかは f が成り立つ
+def finally_(f):
+    # 
+    return True
+
+# パス上のすべての状態でいつも f が成り立つ
+def globally_(f):
+    #
+    return True
+
+# パス上のある状態で g が成り立ち かつ その直前までのすべての状態で f が成り立つ
+def until_(f):
+    #
+    return True
