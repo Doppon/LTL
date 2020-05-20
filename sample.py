86472947f2bcf4f2ce64e0d063bb4dd086533af8
Doppon <zaxpkuraudo7777@gmail.com>
2020-05-20 16:08:59 +0900

    next_のファンクションの処理系を追加

--- sample.py
+++ sample.py
@@ -1,9 +1,17 @@
-# LTL のサンプルプログラム
-
 # パス上の次の状態で f が成り立つ
-def next_(f):
-    #
-    return True
+def next_(path, current_point, f):
+    msg =  "次の状態の真偽 : "  + str(path[current_point + 1][0]) + "\n"
+    msg += "      次の中身 : " + str(path[current_point + 1][1]) + "\n"
+
+    if path[current_point + 1][0]:
+        if f in path[current_point + 1][1]:
+            msg += "          真偽 : " + str(f) + " は Next が成り立つ"
+        else:
+            msg += "          真偽 : " + str(f) + " は Next が成り立たない"
+    else:
+        msg += "          真偽 : " + str(f) + " は Next が成り立たない"
+
+    return msg
 
 # パス上のある状態でいつかは f が成り立つ
 def finally_(f):
@@ -20,5 +28,11 @@ def until_(f):
     #
     return True
 
+
+# LTL のサンプルプログラム
 # 本来は無限
 path = [[False, ["a", "b"]], [False, ["a"]], [True, ["b", "c"]], [False, ["a"]]]
+# 現時点
+current_point = 1
+
+print( next_(path, current_point, "a") )
